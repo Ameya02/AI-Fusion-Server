b37ab7416a7b524b614d1acade0a62c18e9ec09a
Ameya02 <65841021+Ameya02@users.noreply.github.com>
2023-06-13 11:55:41 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const bodyparser = require("body-parser");
+const dotenv = require("dotenv");
+dotenv.config();
+var cors = require("cors");
+const port = process.env.PORT;
+const morgan = require("morgan");
+const http = require("http");
+const server = http.createServer(app);
+
+const connectDB = require("./db");
+
+connectDB();
+const serverRoutes = require("./serverRoutes");
+app.use([
+  cors(),
+  bodyparser.json(),
+  bodyparser.urlencoded({ extended: false }),
+]);
+
+app.get("/", (req, res) => {
+  res.send("Hey this is my API running 🥳");
+});
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+app.use("/api/email", serverRoutes);
